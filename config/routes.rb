4bed4c9ddd33081faeb91ab4c3dc530e38304c26
matt shilling <matthewds@sky.com>
2024-07-06 17:39:41 +0100

    Final version of Rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'tasks', to: 'tasks#index'
-
-  post 'tasks', to: 'tasks#create'
-
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  resources :tasks
 end
